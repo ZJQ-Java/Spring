3e1441cca012f1a8d784a0bb22bed8dd3f3b5064
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-11-14 18:13:53 +0800

    动态代理

--- spring-aop/src/main/java/com/example/proxyTest/JDKProxy.java
+++ spring-aop/src/main/java/com/example/proxyTest/JDKProxy.java
@@ -8,17 +8,21 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 class ProxyInvocationHandler implements InvocationHandler {
-    private Object userDao;
+    private Object target;
 
     public ProxyInvocationHandler(Object userDao) {
-        this.userDao = userDao;
+        this.target = userDao;
+    }
+
+    public Object getProxy() {
+        return Proxy.newProxyInstance(JDKProxy.class.getClassLoader(), target.getClass().getInterfaces(), this);
     }
 
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if (method.getName().startsWith("a")) {
             System.out.println("proxy begin...");
         }
-        Object invoke = method.invoke(userDao, args);
+        Object invoke = method.invoke(target, args);
         System.out.println("proxy after...");
         return invoke;
     }
@@ -26,13 +30,17 @@ class ProxyInvocationHandler implements InvocationHandler {
 
 public class JDKProxy {
     public static void main(String[] args) {
-        Class[] interfaces = new Class[]{UserDao.class};
+        /*Class[] interfaces = new Class[]{UserDao.class};
         UserDao userDao = new UserDaoImpl();
         UserDao userDao1 = (UserDao) Proxy
                 .newProxyInstance(JDKProxy.class.getClassLoader(), interfaces, new ProxyInvocationHandler(userDao));
         int add = userDao1.add(1, 2);
         int subtraction = userDao1.subtraction(1, 2);
         System.out.println(add);
-        System.out.println(subtraction);
+        System.out.println(subtraction);*/
+        ProxyInvocationHandler proxy = new ProxyInvocationHandler(new UserDaoImpl());
+        UserDao user = (UserDao)proxy.getProxy();
+        int add = user.add(1, 2);
+        System.out.println(add);
     }
 }
