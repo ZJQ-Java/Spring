6151e7c1eea36113a810248d0eca7ba2e2b3274d
zhangjinqiu03 <zhangjinqiu03@58.com>
2020-11-23 11:02:12 +0800

    execution

--- spring-aop/src/main/java/com/qiu/aop/UserProxy.java
+++ spring-aop/src/main/java/com/qiu/aop/UserProxy.java
@@ -11,8 +11,14 @@ import org.springframework.stereotype.Component;
 public class UserProxy {
     /**
      * execution([权限修饰符 可省略] [返回类型] [类全路径] [方法名称]([参数列表])  )
+     * execution(* com.sample.service.impl..*.*(..))
+     * 第一个”*“符号	表示返回值的类型任意；
+     * com.sample.service.impl	AOP所切的服务的包名，即，我们的业务部分
+     * 包名后面的”..“	表示当前包及子包
+     * 第二个”*“	表示类名，*即所有类。此处可以自定义，下文有举例
+     * .*(..)	表示任何方法名，括号表示参数，两个点表示任何参数类型
      */
-    @Pointcut(value = "execution(* com.qiu.dao.*.*(..))")
+    @Pointcut(value = "execution(* com.qiu.dao..*.*(..))")
     void pointDemo(){
     }
    /* @Before("execution(* com.example.dao.*.*(..))")
